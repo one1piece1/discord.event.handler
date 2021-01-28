f35435825fc0debdc0e9b86fa2fb211c5a7656c4
one1piece1 <69022551+one1piece1@users.noreply.github.com>
2021-01-28 15:02:43 +0000

    Update intivetracker.js

--- eventos/intivetracker.js
+++ eventos/intivetracker.js
@@ -1,13 +1,9 @@
   
-const client = require('../../index.js');
-
+const client = require('../index.js');
 const { promisify } = require('util');
-
 const wait = promisify(setTimeout);
-
 let invites;
-
-const id = '731875401718300703'; // Id do Servidor
+const id = '000000000000000000'; // Id do Servidor
 
 client.on('ready', async() => {
     await wait(2000);
@@ -22,9 +18,8 @@ client.on('guildMemberAdd', async(member) => {
 
     member.guild.fetchInvites().then(gInvites => {
         const invite = gInvites.find((inv) => invites.get(inv.code).uses < inv.uses);
-
-        const channel = member.guild.channels.cache.get('804362806647980053'); // Canal onde envia
-
+        const channel = member.guild.channels.cache.get('000000000000000000'); // Canal onde envia
+      
         channel.send(`${member} Foi convidado por ${invite.inviter} e o codigo era: ${invite.code}`);
     })
 })
