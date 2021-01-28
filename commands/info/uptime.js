8e0dcba2e0d51b797b25dec12e99c3ccb06deed5
one1piece1 <69022551+one1piece1@users.noreply.github.com>
2021-01-28 15:28:15 +0000

    Update uptime.js

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -5,15 +5,15 @@ module.exports ={
     description: 'BOT Uptime',
     run : async(client, message, args) => {
 
-        let days = Math.floor(client.uptime / 86400000);
-        let hours = Math.floor(client.uptime / 3600000) % 24;
-        let minutes = Math.floor(client.uptime / 60000) % 60;
-        let seconds = Math.floor(client.uptime / 1000) % 60;
+        let Dias = Math.floor(client.uptime / 86400000);
+        let Horas = Math.floor(client.uptime / 3600000) % 24;
+        let Minutos = Math.floor(client.uptime / 60000) % 60;
+        let Segundos = Math.floor(client.uptime / 1000) % 60;
 
         const embed = new MessageEmbed()
         .setTitle('Bot Uptime')
         .setColor('GOLD')
-        .addField('\u200B', `\`Estou Online há\` \`${days}Dias\` \`${hours}Horas\`  \`${minutes}Minutos\` \`${seconds}Segundos\``, true)
+        .addField('\u200B', `\`Estou Online há\` \`${Dias}Dias\` \`${Horas}Horas\`  \`${Minutos}Minutos\` \`${Segundos}Segundos\``, true)
         .setThumbnail('https://i.imgur.com/7FNbSC2.jpg')
 
         message.channel.send(embed)
