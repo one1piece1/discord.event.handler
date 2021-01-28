a4e74565ba7a4c8b0941d94f3e0cfea721c95844
one1piece1 <69022551+one1piece1@users.noreply.github.com>
2021-01-28 15:13:19 +0000

    Update ready.js

--- eventos/ready.js
+++ eventos/ready.js
@@ -1,6 +1,25 @@
 const client = require('../index')
 
 client.on('ready', () => {
-    console.log(`${client.user.username} Website Online`)
+    console.log(`${client.user.username} ✅`)
+    
+    const arrayOfStatus = [
+    `${client.guilds.cache.size} Servidores`,
+    `${client.users.cache.size} Utilizadores`,
+    `${client.channels.cache.size} canais`,
+    `${client.user.tag} Discord bot!`,
+    `Dev by Prolyn3`
+];
+
+    let index = 0;
+    setInterval(() => {
+        if(index === arrayOfStatus.length) index = 0;
+        const status = arrayOfStatus[index];
+        console.log(status);
+        client.user.setActivity(status)
+        index++;
+     }, 5000);
 })
+
+
 
